10290a68388aa2fdb201d1d6669e5d95f1f526dd
mostafa <mostafa.emamdar4@gmail.com>
2020-05-27 06:51:16 +0200

    Changing the Logo Link to Data in all views

--- resources/views/student/studentExam.blade.php
+++ resources/views/student/studentExam.blade.php
@@ -7,7 +7,7 @@
 	<section class="nav-bar">
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div class="container">
-				<a class="navbar-brand" href="#"> <span>A+</span><span> School </span></a>
+				<a class="navbar-brand" href={{route('data')}}> <span>A+</span><span> School </span></a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
