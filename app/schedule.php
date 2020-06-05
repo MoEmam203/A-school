a27e796410fabb1a3b0a069a3dc6e8b79427e904
mostafa <mostafa.emamdar4@gmail.com>
2020-06-05 13:05:35 +0200

    trying to fix schedule error

--- app/schedule.php
+++ app/schedule.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class schedule extends Model
 {
     public function subject(){
-        return $this->hasMany('App\subject');
+        return $this->belongsTo('App\subject');
     }
 }
--- app/subject.php
+++ app/subject.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class subject extends Model
 {
     public function schedule(){
-        return $this->belongsTo('App\schedule');
+        return $this->hasMany('App\schedule');
     }
 }
